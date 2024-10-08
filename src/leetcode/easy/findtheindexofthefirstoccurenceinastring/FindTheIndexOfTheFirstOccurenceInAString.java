3b8d8c8fd8ef95f8065cc043e7107f8e02756abc
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-10-08 17:29:37 -0300

    Completed find the index of the first occurence in a string challenge

---
+++ src/leetcode/easy/findtheindexofthefirstoccurenceinastring/FindTheIndexOfTheFirstOccurenceInAString.java
@@ -0,0 +1,8 @@
+package leetcode.easy.findtheindexofthefirstoccurenceinastring;
+
+public class FindTheIndexOfTheFirstOccurenceInAString {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.strStr("sadbutsad", "sad");
+    }
+}
