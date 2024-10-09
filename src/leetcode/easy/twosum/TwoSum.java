ac6d417aae2aea4488021157e059c5222089488f
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-10-09 10:31:34 -0300

    Updated two sum challenge solution

--- src/leetcode/easy/twosum/TwoSum.java
+++ src/leetcode/easy/twosum/TwoSum.java
@@ -4,10 +4,17 @@ import java.util.Arrays;
 
 public class TwoSum {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        // Customize the array and the target here to see if it works for any case!
-        int[] solutionArr = solution.twoSum(new int[]{3, 2, 4, 7, 14, 21}, 21);
-        System.out.println(Arrays.toString(solutionArr));
+//        Solution solution = new Solution();
+//        // Customize the array and the target here to see if it works for any case!
+//        int[] solutionArr = solution.twoSum(new int[]{3, 2, 4, 7, 14, 21}, 21);
+//        System.out.println(Arrays.toString(solutionArr));
+
+        Solution2 solution2 = new Solution2();
+
+        int[] resultArray = solution2.twoSum(new int[]{3,2,4}, 6);
+
+        System.out.println(Arrays.toString(resultArray));
+
     }
 
 }
