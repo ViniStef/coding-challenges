b9e0ea946ca5b3967a24520cca11cb58768fec3f
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-10-11 15:58:13 -0300

    Completed climbing stairs challenge

---
+++ src/leetcode/easy/climbingstairs/ClimbingStairs.java
@@ -0,0 +1,14 @@
+package leetcode.easy.climbingstairs;
+
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.climbStairs(1));
+        System.out.println(solution.climbStairs(2));
+        System.out.println(solution.climbStairs(3));
+        System.out.println(solution.climbStairs(4));
+        System.out.println(solution.climbStairs(5));
+        System.out.println(solution.climbStairs(6));
+        System.out.println(solution.climbStairs(7));
+    }
+}
